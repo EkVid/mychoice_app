5326ab4f97cadfb738ce29086b13c62859f495fa
Austin <austinjas014@gmail.com>
2026-09-12 22:44:30 -0400

    added const and modified types

---
+++ frontend/src/commons/constants.ts
@@ -0,0 +1,6 @@
+export const DEV_URL = "http://127.0.0.1:8000/";
+export const SIT_URL = "sit_url";
+export const PROD_URL = "prod_url";
+
+export const item = "item";
+export const items = "items";
