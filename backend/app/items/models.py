14d14bdb957543b41c2b894fed05b931bfa53364
Austin <austinjas014@gmail.com>
2026-09-12 21:31:50 -0400

    enforced primary and secondary unique constraints

--- backend/app/items/models.py
+++ backend/app/items/models.py
@@ -5,10 +5,27 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    group = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Group(models.TextChoices):
+        PRIMARY = "P", "Primary"
+        SECONDARY = "S", "Secondary"
+
+    group = models.CharField(
+        max_length=20,
+        choices=Group.choices
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "group"],
+                name="unique_item_each_group"
+            )
+        ]
+
     # turn obj into text
+
     def __str__(self):
         return self.name
