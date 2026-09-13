6c1383af52d61de51cc7383e00e1f6b674f8c8fd
Austin <austinjas014@gmail.com>
2026-09-12 22:02:38 -0400

    api creation

---
+++ frontend/src/api/items.ts
@@ -0,0 +1,16 @@
+import { Item } from "../types/Item";
+
+const config =  {
+    DEV_URL: "http://127.0.0.1:8000/",
+    SIT_URL: ".....",
+    PROD_URL: "....",
+} 
+
+export const getItems = async (): Promise<Item[]> => {
+    const resp = await fetch(`${config.DEV_URL}items/`);
+    if(!resp.ok){
+        throw new Error("Failed to fetch items");
+    }
+
+    return resp.json();
+}
