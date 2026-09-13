a6f28ed942dd1df6d8a7878d689a518c1097b78c
Austin <austinjas014@gmail.com>
2026-09-12 21:02:33 -0400

    created fundamental views

--- backend/app/items/views.py
+++ backend/app/items/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Item
+from .serializers import ItemSerializer
 
 # Create your views here.
+
+
+class ItemCreateView(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+
+class ItemDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
