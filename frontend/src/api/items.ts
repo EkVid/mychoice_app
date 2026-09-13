10d82bfb9cec63978e55df5837fa19cda1214ab2
Austin <austinjas014@gmail.com>
2026-09-12 22:56:25 -0400

    added all APIs

--- frontend/src/api/items.ts
+++ frontend/src/api/items.ts
@@ -3,7 +3,11 @@ import {
     DEV_URL,
     SIT_URL,
     PROD_URL,
-    items
+    ITEMS,
+    FAIL_FETCH,
+    FAIL_CREATE,
+    FAIL_DELETE,
+    FAIL_PATCH
 } from "../commons/constants";
 
 const config =  {
@@ -13,16 +17,16 @@ const config =  {
 } 
 
 export const getItems = async (): Promise<Item[]> => {
-    const resp = await fetch(`${config.DEV_URL}${items}/`);
+    const resp = await fetch(`${config.DEV_URL}${ITEMS}/`);
     if(!resp.ok){
-        throw new Error("Failed to fetch items");
+        throw new Error(FAIL_FETCH);
     }
 
     return resp.json();
 }
 
 export const createItem = async (item: ItemBase): Promise<Item> => {
-    const resp = await fetch(`${config.DEV_URL}${items}/`, {
+    const resp = await fetch(`${config.DEV_URL}${ITEMS}/`, {
         method: "POST",
         headers: {
             "Content-Type" : "application/json",
@@ -31,10 +35,47 @@ export const createItem = async (item: ItemBase): Promise<Item> => {
     });
 
     if(!resp.ok){
-        throw new Error("Failed to create new item");
+        throw new Error(FAIL_CREATE);
     }
 
     const data = await resp.json();
 
     return data;
 } 
+
+export const getItemById = async (id: number) => {
+    const resp = await fetch(`${config.DEV_URL}${ITEMS}/${id}/`);
+    if(!resp.ok){
+        throw new Error(FAIL_FETCH);
+    }
+
+    return resp.json();
+}
+
+export const patchItemById = async (id: number, updated_item: Item): Promise<Item> => {
+    const resp = await fetch(`${config.DEV_URL}${ITEMS}/${id}/`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updated_item)
+    });
+
+    if(!resp.ok){
+        throw new Error(FAIL_PATCH);
+    }
+
+    const data = await resp.json();
+
+    return data;
+}
+
+export const deleteItemById = async (id: number) => {
+    const resp = await fetch(`${config.DEV_URL}${ITEMS}/${id}/`, {
+        method: "DELETE",
+    });
+
+    if(!resp.ok){
+        throw new Error(FAIL_DELETE);
+    }
+}
--- frontend/src/commons/constants.ts
+++ frontend/src/commons/constants.ts
@@ -2,5 +2,10 @@ export const DEV_URL = "http://127.0.0.1:8000/";
 export const SIT_URL = "sit_url";
 export const PROD_URL = "prod_url";
 
-export const item = "item";
-export const items = "items";
+export const ITEM = "item";
+export const ITEMS = "items";
+
+export const FAIL_FETCH = "Failed to fetch item(s)";
+export const FAIL_CREATE = "Failed to create new item";
+export const FAIL_DELETE = "Failed to delete item";
+export const FAIL_PATCH = "Failed to update item";
