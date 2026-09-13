e6fb31fbbcd5898b2cf6d30cc4f71d2b24e6b75d
Austin <austinjas014@gmail.com>
2026-09-12 22:44:38 -0400

    added POST

--- frontend/src/api/items.ts
+++ frontend/src/api/items.ts
@@ -1,16 +1,40 @@
-import type { Item } from "../types/Item";
+import type { Item, ItemBase } from "../commons/types/Item";
+import { 
+    DEV_URL,
+    SIT_URL,
+    PROD_URL,
+    items
+} from "../commons/constants";
 
 const config =  {
-    DEV_URL: "http://127.0.0.1:8000/",
-    SIT_URL: ".....",
-    PROD_URL: "....",
+    DEV_URL: DEV_URL,
+    SIT_URL: SIT_URL,
+    PROD_URL: PROD_URL,
 } 
 
 export const getItems = async (): Promise<Item[]> => {
-    const resp = await fetch(`${config.DEV_URL}items/`);
+    const resp = await fetch(`${config.DEV_URL}${items}/`);
     if(!resp.ok){
         throw new Error("Failed to fetch items");
     }
 
     return resp.json();
 }
+
+export const createItem = async (item: ItemBase): Promise<Item> => {
+    const resp = await fetch(`${config.DEV_URL}${items}/`, {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify(item)
+    });
+
+    if(!resp.ok){
+        throw new Error("Failed to create new item");
+    }
+
+    const data = await resp.json();
+
+    return data;
+} 
