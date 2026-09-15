4753ef869a278c672a670358775996998fc81b1d
Austin <austinjas014@gmail.com>
2026-09-15 16:12:44 -0400

    added validations

--- backend/app/items/serializers.py
+++ backend/app/items/serializers.py
@@ -6,3 +6,10 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = "__all__"
+
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError(
+                "name needs to be non empty"
+            )
+        return value
